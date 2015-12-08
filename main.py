0aa8c1ae82f834de5675819be03606cb38cfe5e4
Eric Grinstein <ericgrinstein@gmail.com>
2015-12-08 23:14:07 +0100

    interface works

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from  database import Database
 
 def main():
    db = Database()
-   #db.set_parking(1,0)
-   #db.set_parking(2,0)
+   db.set_parking(1,0)
+   db.set_parking(2,0)
    m = ParkingModel()
    v = ParkingView(m.status)
    c = ParkingController(m, v)
